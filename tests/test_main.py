7971a2c80984af59c1e9deb955ca7aa8734e2a76
mbradiouf14 <mbradiouf14@gmail.com>
2025-05-24 02:49:02 +0000

    Comment out test_read_main function for future reference

--- tests/test_main.py
+++ tests/test_main.py
@@ -58,10 +58,10 @@ def client():
 # client = TestClient(app)
 
 
-def test_read_main(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World. Welcome to KOZ API"}
+# def test_read_main(client):
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"msg": "Hello World. Welcome to KOZ API"}
     
 def test_read_prompt(client):
     response = client.get("/prompt")
