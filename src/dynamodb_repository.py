a2dc8f0530099349c6aa7b584095f6c29803fc5c
mbradiouf14 <mbradiouf14@gmail.com>
2025-06-02 14:10:09 +0000

    Add UUID generation for message IDs and include 'id' attribute in DynamoDB item

--- src/dynamodb_repository.py
+++ src/dynamodb_repository.py
@@ -1,6 +1,5 @@
 import boto3
-import datetime
-import asyncio
+import datetime, asyncio, uuid
 
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key, Attr
@@ -44,8 +43,9 @@ class DynamoDBRepository:
         Sauvegarde un message (utilisateur ou bot) dans la table DynamoDB.
         """
         try:
-                
+            id = str(uuid.uuid4())  # Génère un UUID
             item = {
+                'id': id,
                 'chat_id': str(chat_id),
                 'timestamp': datetime.datetime.now(datetime.timezone.utc).isoformat(),
                 'message_id': str(message_id),
