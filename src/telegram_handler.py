f777be4bdf7c2ca6789207f3f5197b885b484383
mbradiouf14 <mbradiouf14@gmail.com>
2025-06-03 20:17:05 +0000

    Restore message saving in DynamoDB for start_command and improve error logging

--- src/telegram_handler.py
+++ src/telegram_handler.py
@@ -41,10 +41,7 @@ async def start_command(update: Update, context):
             resize_keyboard=True
         )
         
-        # try:
-        #     await dynamodb_repo.save_message(chat_id, message_id, user.id, user_name, "user", user_message)
-        # except Exception as db_error:
-        #     Utils.log_error(f"DB_ERROR.start_command 1 ====== {e}")
+        await dynamodb_repo.save_message(chat_id, message_id, user.id, user_name, "user", user_message)
         response_text = (
             f"Hello {user_name}! How can I assist you today? Let's have a friendly conversation. Here are a few suggestions for how we can proceed:\n\n"
             "• You can ask me a question about a topic you're interested in.\n"
@@ -55,10 +52,7 @@ async def start_command(update: Update, context):
         )
         # await update.message.reply_text()
         bot_message = await ptb_app.bot.send_message(chat_id=chat_id, text=response_text,  reply_markup=reply_markup)
-        # try:
-        #     await dynamodb_repo.save_message(chat_id, bot_message.message_id, ptb_app.bot.id, ptb_app.bot.username, response_text, "bot")
-        # except Exception as db_error:
-        #     Utils.log_error(f"DB_ERROR.start_command 2 ====== {e}")
+        await dynamodb_repo.save_message(chat_id, bot_message.message_id, ptb_app.bot.id, ptb_app.bot.username, response_text, "bot")
     except Exception as e:
         Utils.log_error(f"Start command error ==== {e}")
     
