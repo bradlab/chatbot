12cfb6ef74c4078404ce8c04000cd721fca4de43
mbradiouf14 <mbradiouf14@gmail.com>
2025-06-03 01:29:01 +0000

    Add warning log for user messages in handle_message function

--- src/telegram_handler.py
+++ src/telegram_handler.py
@@ -61,6 +61,7 @@ async def handle_message(update: Update, context):
             user_name = user.full_name or user.username or "N/A"
             
             try:
+                Utils.log_warning(f"KOZ_MSG ======= {user_name} - {user_message}")
                 await dynamodb_repo.save_message(chat_id, message_id, user.id, user_name, user_message, "user")
             except Exception as db_error:
                 Utils.log_error(f"DB_ERROR.handle_message ====== {e}")
